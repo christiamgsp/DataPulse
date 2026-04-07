9f4da28687bda5b3c89acfb3fa466d77fb9680ca
Briel <christiamgsp@gmail.com>
2026-04-07 14:46:00 +0200

    feat: estructura inicial del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-slate-900 text-white'>
+      <h1 className='text-4xl font-bold text-blue-400'>DataPulse Corriendo</h1>
+      <p className='mt-4 text-slate-300'>
+        Si ves esto en azul y gris, Tailwind v4 funciona.
+      </p>
+    </div>
+  );
+}
+
+export default App;
